78942052b582ada5ef2b2d01a19965fe37862e6a
Epidemy <aipdimi@gmail.com>
2022-06-16 22:34:07 +0430

    Add user to DB bug fixed

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -7,11 +7,17 @@ from pyrogram.types import Message
 @Client.on_message(~filters.edited & ~filters.service, group=1)
 async def users_sql(_, msg: Message):
     if msg.from_user:
-        q = SESSION.query(Users).get(int(msg.from_user.id))
-        if not q:
-            SESSION.add(Users(msg.from_user.id))
-            SESSION.commit()
-        else:
+        try:
+            q = SESSION.query(Users).get(msg.from_user.id)
+            if not q:
+                SESSION.add(Users(msg.from_user.id))
+                SESSION.commit()
+            else:
+                SESSION.close()
+        except:
+            SESSION.rollback()
+            raise
+        finally:
             SESSION.close()
 
 
