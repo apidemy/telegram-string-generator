feab4ec246afc664d9ae1adba7cd1a9b9b813a58
Epidemy <aipdimi@gmail.com>
2022-06-17 13:42:36 +0430

    Initial text updated

--- Data.py
+++ Data.py
@@ -13,7 +13,7 @@ If you don't trust this bot,
 2) block bot or delete chat
 
 This Bot Works To Help You Get Session String Via Bot. Recommendations If You Want To Take String Use Another Account, So As Not To Delay. Thank you
-By @SarfarazStark
+By @ScalpFutures
     """
 
     # Home Button
